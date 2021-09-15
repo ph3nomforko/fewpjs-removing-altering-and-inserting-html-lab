a36ee4d5a2e5b4d828f8ab30105dd76b0fd73521
Codi Kraus <codi.kraus@gmail.com>
2021-09-15 11:41:16 -0500

    commit tree 5bf1707c15e6bbe4542b27f5fdd93126e8d385f0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = ("victory")
+newHeader.innerHTML = ("Codi is the champion")
+newHeader.classname = ("victory")
